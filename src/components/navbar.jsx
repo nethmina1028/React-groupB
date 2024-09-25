fa0a664f025aef383a4e77c405f4ee8c5a16b482
NanayakkaraAR <ashinireshana@gmail.com>
2024-09-25 13:13:03 +0530

    creates nav bar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Logo from '../assets/logo.png'
+function navbar() {
+  return (
+    <div className='my-4 flex flex-row items-center justify-between'>
+      <div className='flex flex-row gap-4 items-center'>
+        <div className='flex flex-row gap-2 items-center'>
+          <img src={Logo} alt="logo" />
+          <h1 className='font-bold'>Landwind</h1>
+        </div>
+      
+         <div>
+        <ul className='flex gap-4 text-sm'>
+         <li>Company</li> 
+         <li>Marketplace</li>
+         <li>Features</li>
+         <li>Team</li>
+         <li>Contact</li>
+        </ul>
+        </div>
+      </div>
+
+
+        <div className='flex flex-row gap-4 items-center text-sm' >
+          <p>Login</p>
+          <p className='bg-purple-700 rounded-lg p-2 text-white'>Get Started</p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default navbar
