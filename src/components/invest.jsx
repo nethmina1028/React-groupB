d48fb84fbea7d2732fe6e09c698b179c6e4c35a9
Yashira-De-Silva <yashiradesilva@gmail.com>
2024-09-29 19:06:12 +0530

    Tools

---
+++ src/components/invest.jsx
@@ -0,0 +1,26 @@
+
+function invest() {
+  return (
+    <div className='flex flex-row  pt-8 items-center gap-4 mx-16'>
+
+      <div className=''>
+        <div className='mb-10'>
+        <h1 className='text-5xl font-bold gap-8'>Invest in the future of development</h1>
+        </div>
+
+      <div>
+        <h1>Work with tools you already use</h1>
+        </div>
+
+        <div>
+             <p>Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work,<br></br> eliminate toil, and deploy changes with ease.</p>
+      </div> 
+        </div>
+      <div>
+      <img src="src\assets\features-1.png" alt="Description of the image" />
+      </div>
+    </div>
+  )
+}
+
+export default invest
